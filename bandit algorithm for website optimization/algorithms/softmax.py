3a07f40b6d49305120f7b386e0b1cf7322d0e21b
高辛格 <xg.gao@tianrang-inc.com>
2017-06-14 20:40:43 +0800

    mab

---
+++ bandit algorithm for website optimization/algorithms/softmax.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import math
+import random
+
+class Softmax(object):
+
+    def __init__(self, temperature, counts, values):
+        self.temperature = temperature
+        self.counts = counts
+        self.values = values
+
+    def initialize(self, n_arms):
+        self.counts = [0 for _ in range(n_arms)]
+        self.values = [0.0 for _ in range(n_arms)]
+
+    def select_arm(self):
+        z = sum([math.exp(v / self.temperature) for v in self.values])
+        probs = [math.exp(v / self.temperature) / z for v in self.values]
+        t = random.random()
+        cum_prob = 0.0
+        for i in range(len(probs)):
+            cum_prob += probs[i]
+            if cum_prob > t:
+                return i
+        return len(probs) - 1
+
+    def update(self, chosen_arm, reward):
+        self.counts[chosen_arm] += 1
+        n = self.counts[chosen_arm]
+        value = self.values[chosen_arm]
+        new_value = ((n-1) * value + reward) / n
+        self.values[chosen_arm] = new_value
+
---
+++ bandit algorithm for website optimization/demo_softmax.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+import random
+from algorithms.softmax import Softmax
+from arms.bernoulli import BernoulliArm
+from tests.test_framework import test_algorithm
+
+random.seed(1)
+means = [0.1, 0.1, 0.1, 0.1, 0.9]
+random.shuffle(means)
+arms = map(lambda mu: BernoulliArm(mu), means)
+
+f = open('demo.tsv', 'w')
+algo = Softmax(0.1, [], [])
+results = test_algorithm(algo, arms, 5000, 250)
+for i in range(len(results[0])):
+    f.write(str(0.1) + '\t')
+    f.write('\t'.join([str(results[j][i]) for j in range(len(results))]) + '\n')
+f.close()
