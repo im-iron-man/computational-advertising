389753ffd7f9eb1915ddb39c078f5d346a25e358
高辛格 <xg.gao@tianrang-inc.com>
2017-06-14 21:33:53 +0800

    mab

---
+++ bandit algorithm for website optimization/algorithms/annealing_epsilon_greedy.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import math
+import random
+
+class AnnealingEpsilonGreedy(object):
+
+    def __init__(self, counts, values):
+        self.counts = counts
+        self.values = values
+
+    def initialize(self, n_arms):
+        self.counts = [0 for _ in range(n_arms)]
+        self.values = [0.0 for _ in range(n_arms)]
+
+    def select_arm(self):
+        t = sum(self.counts) + 1
+        epsilon = 1 / math.log(t + 0.0000001)
+        if random.random() > epsilon:
+            m = max(self.values)
+            return self.values.index(m)
+        else:
+            return random.randrange(len(self.values))
+
+    def update(self, chosen_arm, reward):
+        self.counts[chosen_arm] += 1
+        n = self.counts[chosen_arm]
+        value = self.values[chosen_arm]
+        new_value = ((n-1) * value + reward) / n
+        self.values[chosen_arm] = new_value
