617a1702b055cc7946bcc8dfd3102076fb657797
高辛格 <xg.gao@tianrang-inc.com>
2017-07-06 21:42:59 +0800

    ensemble method

---
+++ Ensemble Methods Foundations and Algorithms/tests/random_forest.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
+
+# 步骤一：明确分类器
+clf = RandomForestClassifier(n_estimators=10, max_depth=None, min_samples_split=2, random_state=0)
+
+# 步骤二：训练
+iris = datasets.load_iris()
+X = iris.data[:100, :2]
+y = iris.target[:100]
+clf.fit(X, y)
+
+# 步骤三：预测
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, .02), np.arange(y_min, y_max, .02))
+Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+ZZ = Z.reshape(xx.shape)
+
+# 步骤四：绘图
+cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+plt.figure()
+plt.pcolormesh(xx, yy, ZZ, cmap=cmap_light)
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold)
+plt.xlim(xx.min(), xx.max())
+plt.ylim(yy.min(), yy.max())
+plt.show()
