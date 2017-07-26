d9d06a87c7932c665242be2dc99a82dc0035e764
高辛格 <xg.gao@tianrang-inc.com>
2017-07-26 21:52:11 +0800

    machine learning

--- machine learning/tensorflow/5/a.py
+++ machine learning/tensorflow/5/a.py
@@ -25,7 +25,19 @@ train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.scatter(x_data, y_data)
+plt.ion()
+plt.show()
 for _ in range(1000):
     sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
     if _ % 50 == 0:
-        print(sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
+        # print(sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
+        try:
+            ax.lines.remove(lines[0])
+        except:
+            pass
+        prediction_value = sess.run(prediction, feed_dict={xs: x_data})
+        lines = ax.plot(x_data, prediction_value, 'r-', lw=5)
+        plt.pause(0.1)
