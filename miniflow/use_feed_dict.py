93465902fc00e16d45aef175c6caf5e053d726de
高辛格 <xg.gao@tianrang-inc.com>
2017-08-05 10:06:43 +0800

    delete miniflow

--- miniflow/use_feed_dict.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import miniflow.miniflow as tf
-
-sess = tf.Session()
-
-a = tf.placeholder(tf.float32)
-b = tf.constant(32.0)
-print(sess.run(a + b, feed_dict={a: 10}))
-print(sess.run(a + b, feed_dict={a.name: 10}))
