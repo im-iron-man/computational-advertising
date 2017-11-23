f442eae947d1c53708f522127cee2401e2ff02ad
高辛格 <xg.gao@tianrang-inc.com>
2017-11-23 13:16:19 +0800

    machine learning2

--- machine learning2/test1.py
+++ machine learning2/test1.py
@@ -7,7 +7,45 @@ class TableAgent:
         self.reward = reward_table
         self.state_num = self.table.shape[1]
         self.act_num = self.table.shape[0]
-        self.policy = no.zeros(self.state_num, dtype=np.int)
+        self.policy = np.zeros(self.state_num, dtype=np.int)
         self.value_pi = np.zeros((self.state_num))
         self.value_q = np.zeros((self.state_num, self.act_num))
         self.gamma = 0.8
+        
+    def policy_evaluation(self):
+        while True:
+            new_value_pi = self.value_pi.copy()
+            for i in range(1, self.state_num):
+                value_sas = []
+                for j in range(0, self.act_num):
+                    value_sa = np.dot(self.table[j, i, :], self.reward + self.gamma * self.value_pi)
+                    value_sas.append(value_sa)
+                new_value_pi[i] = value_sas[self.policy[i]]
+            diff = np.sqrt(np.sum(np.power(self.value_pi - new_value_pi, 2)))
+            if diff < 1e-6:
+                break
+            else:
+                self.value_pi = new_value_pi
+                
+    def policy_improvement(self):
+        new_policy = np.zeros_like(self.policy)
+        for i in range(1, self.state_num):
+            for j in range(0, self.act_num):
+                self.value_q[i, j] = np.dot(self.table[j, i, :], self.reward + self.gamma * self.value_pi)
+                max_act = np.argmax(self.value_q[i, :])
+                new_policy[i] = max_act
+        if np.all(np.equal(new_policy, self.policy)):
+            return False
+        else:
+            self.policy = new_policy
+            return True
+                    
+    def policy_iteration(self):
+        iteration = 0
+        while True:
+            iteration += 1
+            self.policy_evaluation()
+            ret = self.policy_improvement()
+            if not ret:
+                break
+            print 'Iter {} rounds coverge'.format(iteration)
---
+++ machine learning2/test2.py
@@ -0,0 +1,46 @@
+from test import Snake
+from test1 import TableAgent
+
+def eval(env, agent):
+    r = 0
+    env.start()
+    while True:
+        a = agent.policy[env.pos]
+        reward, state = env.action(a)
+        r += reward
+        if state == -1:
+            break
+    return r
+    
+def simple_eval():
+    env = Snake(0, [3, 6])
+    agent = TableAgent(env.state_transition_table(), env.reward_table())
+    print 'return3={}'.format(eval(env, agent))
+    agent.policy[:] = 1
+    print 'return6={}'.format(eval(env, agent))
+    agent.policy[97:100] = 0
+    print 'return_ensemeble={}'.format(eval(env, agent))
+    
+def policy_iteration_demo():
+    env = Snake(0, [3, 6])
+    agent = TableAgent(env.state_transition_table(), env.reward_table())
+    agent.policy_iteration()
+    print 'return_pi={}'.format(eval(env, agent))
+    print agent.policy
+    
+def policy_iteration_demo2():
+    env = Snake(10, [3,6])
+    agent = TableAgent(env.state_transition_table(), env.reward_table())
+    print 'return3={}'.format(eval(env,agent))
+    agent.policy[:] = 1
+    print 'return6={}'.format(eval(env,agent))
+    agent.policy[97:100] = 0
+    print 'return_ensemble={}'.format(eval(env,agent))
+    agent.policy_iteration()
+    print 'return_pi={}'.format(eval(env,agent))
+    print agent.policy
+
+if __name__ == '__main__':
+    simple_eval()
+    policy_iteration_demo()
+    policy_iteration_demo2()
